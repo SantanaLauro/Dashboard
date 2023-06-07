44f9eb2ee13810f802cd740e8bb724c30f713b3b
SantanaLauro <126792216+SantanaLauro@users.noreply.github.com>
2023-06-07 15:59:31 -0300

    Update example.js

--- example.js
+++ example.js
@@ -9,7 +9,7 @@ function main() {
 			   .attr("x", 50)
 			   .attr("y", 50)
 			   .attr("font-size", "24px")
-			   .text("Stock Price")
+			   .text("Stock Pricessss")
 		
 			var xScale = d3.scaleBand().range([0, width]).padding(0.4),
 				yScale = d3.scaleLinear().range([height, 0]);
